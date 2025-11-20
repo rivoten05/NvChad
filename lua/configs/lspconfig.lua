bdf4f46a7db148b1d8e7aee503339ca8d48e54e1
rivoten <rivoten05@gmail.com>
2025-11-20 18:17:44 +0530

    lsp config

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
+local enabled_servers = {
+  "lua_ls",
+  "rust_analyzer",
+}
+vim.lsp.enable(enabled_servers)
 
-local servers = { "lua_ls" }
-vim.lsp.enable(servers)
-
-local rust = { "rust_analyzer" }
-vim.lsp.enable(rust)
--- read :h vim.lsp.config for changing options of lsp servers
 vim.lsp.config("rust_analyzer", {
   settings = {
     ["rust-analyzer"] = {
