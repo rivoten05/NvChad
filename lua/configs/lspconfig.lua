d63af86f1d31088ec3311d3d23fde9a01dd72a78
rivoten <rivoten05@gmail.com>
2025-11-20 17:23:40 +0530

    add rust lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -3,4 +3,15 @@ require("nvchad.configs.lspconfig").defaults()
 local servers = { "lua_ls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+local rust = { "rust_analyzer" }
+vim.lsp.enable(rust)
+-- read :h vim.lsp.config for changing options of lsp servers
+vim.lsp.config("rust_analyzer", {
+  settings = {
+    ["rust-analyzer"] = {
+      diagnostics = {
+        enable = true,
+      },
+    },
+  },
+})
