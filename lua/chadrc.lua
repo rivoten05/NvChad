624baa338e96752b4b3e9d567242bdb5b64744e1
rivoten <rivoten05@gmail.com>
2025-11-20 15:21:48 +0530

    NvChad initial setup

---
+++ lua/chadrc.lua
@@ -0,0 +1,20 @@
+---@type ChadrcConfig
+local M = {}
+
+M.base46 = {
+	theme = "ashes",
+
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+}
+
+M.nvdash = { load_on_startup = true }
+M.ui = {
+      tabufline = {
+         lazyload = false
+     }
+}
+
+return M
