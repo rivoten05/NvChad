921594da9de20b5b0bef9d63207836856c96506d
rivoten <rivoten05@gmail.com>
2025-11-20 16:21:10 +0530

    highlight yank

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -16,3 +16,23 @@ autocmd("BufReadPost", {
     end
   end,
 })
+
+vim.api.nvim_create_autocmd("BufDelete", {
+  callback = function()
+    local bufs = vim.t.bufs
+    if #bufs == 1 and vim.api.nvim_buf_get_name(bufs[1]) == "" then
+      vim.cmd "Nvdash"
+    end
+  end,
+})
+
+local function augroup(name)
+  return vim.api.nvim_create_augroup("lazyvim_" .. name, { clear = true })
+end
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = augroup "highlight_yank",
+  callback = function()
+    (vim.hl or vim.highlight).on_yank()
+  end,
+})
