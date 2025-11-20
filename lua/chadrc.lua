bc5f0fd909beff222fff5c5a8fb248756661bdda
rivoten <rivoten05@gmail.com>
2025-11-20 15:45:08 +0530

    noice

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 M.base46 = {
-	theme = "ashes",
+	theme = "kanagawa",
 
 	hl_override = {
 		Comment = { italic = true },
---
+++ lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  event = "VimEnter",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "rcarriga/nvim-notify",
+  },
+  opts = {},
+  config = function()
+    require("core.noice").setup()
+  end,
+}
