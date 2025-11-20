1b32e1fb74b85ad66896099fa968cd73da566b2b
rivoten <rivoten05@gmail.com>
2025-11-20 16:27:35 +0530

    blink-indent

--- lua/plugins/dressing.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "saghen/blink.indent",
-  version = "*",
-  dependencies = "saghen/blink.download",
-  event = { "BufReadPost", "BufNewFile" },
-  -- @module 'blink.indent',
-  -- @type 'blink.indent.Config'
-}
