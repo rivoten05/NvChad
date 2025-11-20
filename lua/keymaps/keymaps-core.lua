d6940bf8f3f4471378a78580ef5adfcb216270bf
rivoten <rivoten05@gmail.com>
2025-11-20 16:34:14 +0530

    resize window

--- lua/keymaps/keymaps-core.lua
+++ lua/keymaps/keymaps-core.lua
@@ -5,3 +5,9 @@ map("i", "<C-j>", "<Down>", { desc = "Move Down in Insert" })
 map("i", "<C-k>", "<Up>", { desc = "Move Up in Insert" })
 map("i", "<C-l>", "<Right>", { desc = "Move Right in Insert" })
 map("i", "<C-b>", "<ESC>^i", { desc = "Move to Beginning of Line in Insert" })
+
+-- Resize window using leader + arrow keys
+map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
